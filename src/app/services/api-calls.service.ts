3ad1c36cd9d095cb598f97000d690776a208d5e5
jayeshbhanderi <jayeshbhanderi30@gmail.com>
2023-07-28 10:52:29 +0530

    token fix

--- src/app/services/api-calls.service.ts
+++ src/app/services/api-calls.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import EndPoints from '../common/endpoints';
 import { AuthModel } from '../modules/auth/models/auth.model';
 import { environment } from 'src/environments/environment';
+import { Utils } from './utils';
 
 @Injectable({
   providedIn: 'root',
@@ -11,56 +12,52 @@ import { environment } from 'src/environments/environment';
 export class ApiCallsService {
   private authLocalStorageToken = `${environment.appVersion}-${environment.USERDATA_KEY}`;
   host_url = environment.apiUrl;
-  _headers: any;
-  constructor(private http: HttpClient) {
-    const token: any = this.getAuthFromLocalStorage();
-    this._headers = new HttpHeaders({
-      ['Auth-Token']: token.token,
-    });
-  }
+
+  constructor(private http: HttpClient, private utils: Utils) {}
 
   get(endpoint: any, queryParams?: any): Observable<any> {
+    const token: any = this.utils.getAuth();
+    const _headers = new HttpHeaders({
+      ['Auth-Token']: token.token,
+    });
     return this.http.get(`${this.host_url}${endpoint}`, {
       params: queryParams,
-      headers: this._headers,
+      headers: _headers,
     });
   }
 
   getDocument(endpoint: string, queryParams?: any): Observable<Blob> {
+    const token: any = this.utils.getAuth();
+    const _headers = new HttpHeaders({
+      ['Auth-Token']: token.token,
+    });
     return this.http.get(`${this.host_url}${endpoint}`, {
       params: queryParams,
-      headers: this._headers,
+      headers: _headers,
       responseType: 'blob',
     });
   }
 
   post(endpoint: any, data?: any, queryParam?: any): Observable<any> {
+    const token: any = this.utils.getAuth();
+    const _headers = new HttpHeaders({
+      ['Auth-Token']: token.token,
+    });
     return this.http.post(`${this.host_url}${endpoint}`, data, {
       params: queryParam,
-      headers: this._headers,
+      headers: _headers,
     });
   }
 
   delete(endpoint: any, queryParams?: any): Observable<any> {
+    const token: any = this.utils.getAuth();
+    const _headers = new HttpHeaders({
+      ['Auth-Token']: token.token,
+    });
     return this.http.delete(`${this.host_url}${endpoint}`, {
       params: queryParams,
-      headers: this._headers
+      headers: _headers,
     });
-  }
-
-  private getAuthFromLocalStorage(): AuthModel | undefined {
-    try {
-      const lsValue = localStorage.getItem(this.authLocalStorageToken);
-      if (!lsValue) {
-        return undefined;
-      }
-
-      const authData = JSON.parse(lsValue);
-      return authData;
-    } catch (error) {
-      console.error(error);
-      return undefined;
-    }
   }
 
   // { 'headers': { "Content-Type": "multipart/form-data" } }
